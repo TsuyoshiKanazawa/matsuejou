e2db29918767d9b41bd49061418fa77e743b0161
TsuyoshiKanazawa <t.kanazawa0810@gmail.com>
2025-02-21 19:00:39 +0900

    mod

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -31,12 +31,12 @@ export default defineNuxtConfig({
         { hid: 'og:url', property: 'og:url', content: 'https://navi.kankou-matsue.jp/matsue-castle-kouryakushitsu/' },
         { hid: 'og:title', property: 'og:title', content: '松江城攻略室 | 国宝指定10周年記念オリジナルブラウザゲーム' },
         { hid: 'og:description', property: 'og:description', content: '島根県松江市にある国宝松江城天守が2025年7月8日「国宝指定10周年」を迎えることを記念し登場した高難易度オリジナルブラウザゲーム。松江城が誇る「防御性能の高さ」を全5ステージ＋シークレットステージで体験できるほか、松江城の見所も紹介！| 一般社団法人松江観光協会' },
-        { hid: 'og:image', property: 'og:image', content: '/img/OGP.jpg' },
+        { hid: 'og:image', property: 'og:image', content: '/OGP.jpg' },
         { hid: 'twitter:card', property: 'twitter:card', content: 'summary' },
         { hid: 'twitter:site', property: 'twitter:site', content: '@' },
         { hid: 'twitter:title', property: 'twitter:title', content: '松江城攻略室 | 国宝指定10周年記念オリジナルブラウザゲーム' },
         { hid: 'twitter:description', property: 'twitter:description', content: '島根県松江市にある国宝松江城天守が2025年7月8日「国宝指定10周年」を迎えることを記念し登場した高難易度オリジナルブラウザゲーム。松江城が誇る「防御性能の高さ」を全5ステージ＋シークレットステージで体験できるほか、松江城の見所も紹介！| 一般社団法人松江観光協会' },
-        { hid: 'twitter:image', property: 'twitter:image', content: '/img/OGP-square.jpg' }
+        { hid: 'twitter:image', property: 'twitter:image', content: '/OGP-square.jpg' }
       ],
       link: [
         { rel: 'icon', type: 'image/x-icon', href: '/matsue-castle-kouryakushitsu/favicon.ico' }
